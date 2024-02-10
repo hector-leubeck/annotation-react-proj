9132e8a890c286e1a84b8269a3481357e5f1b92e
hector-leubeck <hector.leubeck.lux.renatus@gmail.com>
2024-02-10 19:14:10 -0300

    Feat: Create Modal Component

--- src/components/NoteCards.tsx
+++ src/components/NoteCards.tsx
@@ -1,15 +1,28 @@
-type  NoteCardsProps = {
-    noteDate: string;
-    text: string;
-} 
+import * as Dialog from "@radix-ui/react-dialog";
 
-export const NoteCards = (props: NoteCardsProps) => 
-    <button className="rounded-md text-left bg-slate-800 p-5  outline-none space-y-3 overflow-hidden relative hover: ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+type NoteCardsProps = {
+  note: {
+    date: Date;
+    text: String;
+  };
+};
+
+export const NoteCards = ({ note }: NoteCardsProps) => {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left bg-slate-800 p-5  outline-none flex flex-col gap-3 overflow-hidden relative hover: ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-200">
-            {props.noteDate}
-            </span>
-        <p className="text-sm leading-6 text-slate-300">
-            {props.text}
-            </p>
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-300">{note.text}</p>
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/60 z-[2]" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+          Hello World
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
