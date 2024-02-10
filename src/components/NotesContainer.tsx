45d560d15319cc1fa0d756c5c7e391285e11b6fa
hector-leubeck <hector.leubeck.lux.renatus@gmail.com>
2024-02-10 19:13:38 -0300

    Refactpr: Change Proptype of date [string to Date]

--- src/components/NotesContainer.tsx
+++ src/components/NotesContainer.tsx
@@ -1,16 +1,21 @@
 import { NewNoteCard } from "./NewNoteCard"
 import { NoteCards } from "./NoteCards"
 
+const dateExample = {
+    date: new Date(),
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum minus reiciendis velit sunt quia fugiat, sed distinctio magnam dolorum nam id laudantium nostrum, reprehenderit eaque ducimus molestias tempore aut iusto.Pariatur laboriosam voluptatem nihil et nulla molestiae eaque unde deleniti ab aliquid facere soluta necessitatibus consequuntur, doloremque distinctio aspernatur excepturi neque? Nobis porro voluptatum dolor cum perferendis consectetur in molestias."
+}
+
 export const NotesContainer = () => 
     <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCards noteDate="há 2 dias" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eligendi laborum! Reiciendis eligendi magnam optio assumenda laborum aliquid molestiae officiis hic praesentium sequi eius ex quam, sint et non voluptate?"/>
-
-        <NoteCards noteDate="há 4 dias" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eligendi laborum! Reiciendis eligendi magnam optio assumenda laborum aliquid molestiae officiis hic praesentium sequi eius ex quam, sint et non voluptate?"/>
-
-        <NoteCards noteDate="há 5 dias" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eligendi laborum! Reiciendis eligendi magnam optio assumenda laborum aliquid molestiae officiis hic praesentium sequi eius ex quam, sint et non voluptate?"/>
-        <NoteCards noteDate="há 5dias" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eligendi laborum! Reiciendis eligendi magnam optio assumenda laborum aliquid molestiae officiis hic praesentium sequi eius ex quam, sint et non voluptate?"/>
-        <NoteCards noteDate="há 1 semana" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eligendi laborum! Reiciendis eligendi magnam optio assumenda laborum aliquid molestiae officiis hic praesentium sequi eius ex quam, sint et non voluptate?"/>
-        <NoteCards noteDate="há 6 mêses" text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, eligendi laborum! Reiciendis eligendi magnam optio assumenda laborum aliquid molestiae officiis hic praesentium sequi eius ex quam, sint et non voluptate?"/>
+        <NoteCards note={dateExample}/>
+        <NoteCards note={dateExample}/>
+        <NoteCards note={dateExample}/>
+        <NoteCards note={dateExample}/>
+        <NoteCards note={dateExample}/>
+        <NoteCards note={dateExample}/>
+        <NoteCards note={dateExample}/>
+        <NoteCards note={dateExample}/>
     </div>
 
